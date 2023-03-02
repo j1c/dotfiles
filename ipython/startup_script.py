2ff7b3816e0d083292bccf44498c7e7b119a0dc6
j1c <jaewonc78@gmail.com>
2023-03-02 13:37:22 -0500

    Update to load favorite packages

--- ipython/startup_script.py
+++ ipython/startup_script.py
@@ -1,2 +1,9 @@
 get_ipython().magic(u"%load_ext autoreload")
 get_ipython().magic(u"%autoreload 2")
+
+# Load favorite packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
